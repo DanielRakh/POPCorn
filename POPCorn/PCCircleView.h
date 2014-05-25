035f7cb8ffaaec46ebcf85fbbe376485bd783194
Daniel Rakhamimov <danielrakh@gmail.com>
2014-05-25 15:40:34 -0400

    PCCircleView added.

---
+++ POPCorn/PCCircleView.h
@@ -0,0 +1,15 @@
+//
+//  PCCircleView.h
+//  POPCorn
+//
+//  Created by Daniel Rakhamimov on 5/25/14.
+//  Copyright (c) 2014 Daniel Rakhamimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//@abstract: PCCircleView is a UIView that is a a perfect circle with: **to be added later**
+
+@interface PCCircleView : UIView
+
+@end
---
+++ POPCorn/PCCircleView.m
@@ -0,0 +1,44 @@
+//
+//  PCCircleView.m
+//  POPCorn
+//
+//  Created by Daniel Rakhamimov on 5/25/14.
+//  Copyright (c) 2014 Daniel Rakhamimov. All rights reserved.
+//
+
+#import "PCCircleView.h"
+
+@implementation PCCircleView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self commonInit];
+    }
+    return self;
+}
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    [self commonInit];
+}
+
+- (void)commonInit {
+    [self roundEdgesToMakeCircle];
+}
+
+- (void)roundEdgesToMakeCircle {
+    self.layer.cornerRadius = self.bounds.size.width/2.0;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
