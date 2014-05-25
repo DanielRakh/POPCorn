2a432a7e53fbb05ee371b0c1444ca564312db3da
Daniel Rakhamimov <danielrakh@gmail.com>
2014-05-25 15:17:36 -0400

    Scale VC created.

---
+++ POPCorn/PCScaleViewController.h
@@ -0,0 +1,13 @@
+//
+//  PCScaleViewController.h
+//  POPCorn
+//
+//  Created by Daniel Rakhamimov on 5/25/14.
+//  Copyright (c) 2014 Daniel Rakhamimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PCScaleViewController : UIViewController
+
+@end
